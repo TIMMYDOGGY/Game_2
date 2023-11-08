6e444338a47aa02ea1b7c9cb3c8608583b933be6
TIMMYDOGGY <131836888+TIMMYDOGGY@users.noreply.github.com>
2023-11-08 17:13:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,153 @@
+import random
+randomnumber0 = random.randint(1, 10000)
+print("Wellcome to basically minecraft!")
+print("Use 1 or 2 or 3 or 4 or 4 or any number to decide which thing you wanna do.")
+Name1 = input("Whats ya name?")
+Name0 = input("OKAY Wellcome What are you gonna name your world?")
+print(f"Got it {Name0} then. Not the best name but sure. Try your best {Name1}!")
+print("Loading...")
+print("Loading...")
+print("Loading...")
+while True:
+  First = input("Okay you loaded into the new World! You spawned in the middle of a oak forest. What are you do? (Break down trees or Kill animals)")
+  if First == "1":
+    print("You cut down every single tree in the entire forest. Your inventory is breaking with wood.")
+    Second = input("What are you gonna do now? (Mine for dirt or Travel?")
+    if Second == "1":
+      print(f"Well you mined a 100 by 100 place just looking for coal even though you passed dimands and mob spawner instead you found {randomnumber0} stacks of just DIRT.")
+      Third = input("Good job I guess? Now you have the BIG choice (Go fight the enderdragen with DIRT armer or Keep mining for something useful)")
+      if Third == "1":
+        print(f"I am so sorry {Name1}. I forgot that you need to have blaze powder and enderpearls that cannot be made of DIRT.")
+        Forth = input("You need to go to the neather for those carp. (Go find a lava pool or build amazing base entirely built out of DIRT")
+        if Forth =="1":
+          print("You find one instantly!")
+          Five = input("You clutch up using your DIRT tools and DIRT buckets. And build the neather pearls.(Go in or Build skybase)")
+          if Five == "1":
+            print("You spawned right in the middle of a neather fortress???")
+            Six = input("YOOO okay okay do you wanna (Kill blazes or Go to the next door bastion)")
+            if Six == "1":
+              print("Yes okay you kill millions and millions of blazes , wither skelitons, and enderman")
+              Seven = input("Why just why did you kill so much? I really do like killing. Now what do you do know? (Kill the ender dragon or Gear up in the overworld)")
+              if Seven == "1":
+                print("I like this lets go.")
+                Eight = input("You make 10,000 Eyes of Ender with your infenite amount of loot. (Go kill enderdragon, Find dogs)")
+                if Eight == "1":
+                  print("You manege to find the portal room first try by digging in to it.")
+                  Nine = input("You put in the Eyes of Ender. And you go in. (Make 1,000 withers or kill the enderdragon using you dirt tools)")
+                  if Nine == "1":
+                    print("Okay then.")
+                    Ten = input("The enderdragon is low but so are you. (Go in for the kill or wait it out)")
+                    if Ten == "1":
+                      print(f"Congratlations {Name1} you beat the game! This game has a few criterea for winning.")
+                      print("Killing : 40%")
+                      print("kindness : 30%")
+                      print("Evilness : 30%")
+                      print("Explored : 20% ")
+                      print("Alive : 60%")
+                      print("TOTAL SCORE : 180/500")
+                    elif Ten == "2":
+                      print("You wait it out. Like a whimp.")
+                      print("After hours and hours the enderdragon dies. But by the time you died from hunger.")
+                      print(f"Congratlations {Name1} you beat the game (Kinda)! This game has a few criterea for winning.")
+                      print("Killing : 40%")
+                      print("kindness : 30%")
+                      print("Evilness : 30%")
+                      print("Explored : 20% ")
+                      print("Alive : 00%")
+                      print("TOTAL SCORE : 120/500")
+                  elif Nine == "2":
+                    print("You start the grulling process of hitting the dragon over and over again with your dirt.")
+                    print("After the longest of time you finally beat the dragon")
+                    print(f"Congratlations {Name1} you beat the game (Kinda)! This game has a few criterea for winning.")
+                    print("Killing : 40%")
+                    print("kindness : 10%")
+                    print("Evilness : 30%")
+                    print("Explored : 20% ")
+                    print("Alive : 20%")
+                    print("TOTAL SCORE : 120/500")
+                elif Eight == "2":
+                  print("Why just WHY?")
+                  Eleven = input("You explored the entire world looking for dogs then breeding them. BY GOD (Go fight the enderdragon or Fight withers")
+                  if Eleven == "1":
+                    print("You go to the portal room somewhat easily takening some time looking for it")
+                    print("Using your dogs you manage to kill the enderdragon at time breaking speed")
+                    print(f"Yay {Name1} you beat the game! This game has a few criterea for winning.")
+                    print("Killing : 40%")
+                    print("kindness : 30%")
+                    print("Evilness : 20%")
+                    print("Explored : 80% ")
+                    print("Alive : 70%")
+                    print("TOTAL SCORE : 240/500")
+                  elif Eleven == "2":
+                    print("You, you killed all of them. You made 3,000 withers at once and killed them all at once. You aren't alive anymore ")
+                    print("You stink of death")
+                    print("You go to the portal room and enter. The second you enter the enderdragon dies from the stench of DEATH and EVIL coming from you.")
+                    print(f"Good job {Name1} you beat the game. This game has a few criterea for winning.")
+                    print("Killing : 80%")
+                    print("kindness : 00%")
+                    print("Evilness : 60%")
+                    print("Explored : 80% ")
+                    print("Alive : 00%")
+                    print("TOTAL SCORE : 220/500")
+              elif Seven == "1":
+                print("After the longest of time. You have full diamond armor with max echantmants.")
+                Twelve = input("You are unkillable (Go kill the enderdragon")
+                if Twelve == "1":
+                  print("You go into the portal after awhile of looking through. You enter the portal")
+                  print("When you walk to the middle of the end island the dragon comes right at you then stops right in it's tracks like it ran into a wall.")
+                  print(f"You barely fought anyhting {Name1} but you did beat the game. This game has a few criterea for winning.")
+                  print("Killing : 30%")
+                  print("kindness : 20%")
+                  print("Evilness : 10%")
+                  print("Explored : 70% ")
+                  print("Alive : 100%")
+                  print("TOTAL SCORE : 230/500")
+                elif Twelve =="2":
+                  print("That, thats not a optain. how did you. You chose the deleted second option ENSlAVE THE WORLD")
+                  print("Well then. You go around the world killing everything in sight and enslaving all the villagers. Then you destroy the neather untill there is nothing left.")
+                  print("you enter the portal room with the world burning behind you")
+                  print("When you enter the dragon dove at you aiming right for your head. It did not even get to register your sword before it died.")
+                  print(" No I refuse to let you have a good score. You did beat the game but at what cost. This game has a few criterea for winning and you should not even get to look at them.")
+                  print("Killing : 100%")
+                  print("kindness : 00%")
+                  print("Evilness : 100%")
+                  print("Explored : 100% ")
+                  print("Alive : -100%")
+                  print("TOTAL SCORE : 200/500")
+            elif Six == "2":
+              print("You walk into the bastian and steal all of their gold but you are wearing dirt armor so the piglens attack you.")
+              Thirteen = input("You have two optains here before you get tossed in the lava (Trade the gold or Bribe them)")
+              if Thirteen == "1":
+                print("You Trade away all of your gold then quickly make gold boots and put them on.")
+                seventeen = input("You have tons of everything you would ever need. (Defeat the neather fortress or Yoink more stuff from the piglins)")
+                if seventeen == "1":
+                  print("You defeat the fortress and now you have enough to completly beat the dragon.")
+                sixteen = input("Exept armor. (Go for it or Mine for extra stuff)")
+                if sixteen == "1":
+                  print("You head to the portal room. And enter it. Then remember that you forgot your sword.")
+                  print("Well then at least you have dirt.")
+                  print("You just start to slap the dragon with dirt over and over again")
+                  print("It takes hour but you did it cause of how much dirt you have")
+                  print("You did beat the game just really slow. This game has a few criterea for winning.")
+                  print("Killing : 50%")
+                  print("kindness : 30%")
+                  print("Evilness : 30%")
+                  print("Explored : 20% ")
+                  print("Alive : 20%")
+                  print("TOTAL SCORE : 150/500")
+                  Fifteen = input("You have everything you need. You and your army enter the overworld()") 
+
+
+  else:
+    print("That is not a optain! YOU DIED")
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# NAME OF YOUR GAME
+
+## Descriptions
+
+Briefly describe the premise of your game to give potential players a sense of what it will be like to play.
+
+## Flow chart
+
+Sketch a flow chart of your game. What are the options the user can choose between, and where will those choices lead them? You might want to draw this on paper, take a picture, and upload that here.
